74ade4d2ae00edfa0674b95871373d222e71dad1
TheArctesian <87273292+TheArctesian@users.noreply.github.com>
2021-10-07 11:59:32 +0800

    working poggers

--- imgseq.js
+++ imgseq.js
@@ -1,25 +1,47 @@
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
 $('#seq').hover(
-    function () {
-        for (var i = 1; i < 175; i++){
+    async function () {
+        for (var i = 1; i < 175; i++) {
             var fra = i;
             // console.log(fra.toString().length);
-            if (fra.toString().length == 1){
-                frame = "00"+fra; 
-                console.log(frame);
-            }
-            else if (fra.toString().length == 2){
-                frame = "0"+fra;
-            }
-            else if (fra.toString().length == 3){
+            if (fra.toString().length == 1) {
+                frame = "00" + fra;
+            } else if (fra.toString().length == 2) {
+                frame = "0" + fra;
+            } else if (fra.toString().length == 3) {
                 frame = fra;
             }
-            console.log(frame);
-            var imgSeqAddress = "UNLTRASTABLE-flip-pngSeq/tokenFlip-video0" + frame + ".png";
+            var imgSeqAddress = "ultrastable-rain-flip-pngSwq/tokenFlip-video0" + frame + ".png";
             $("#seq").attr("src", imgSeqAddress);
-            await timer(2.5); //because anim is in 24 fps so 60/24 = 2.5
+            await timer(200); //because anim is in 24 fps so 60/24 = 2.5
         }
         console.log("done");
+    }
+);
+
+$('#coin').hover(
+    async function () {
+        console.log($(window).width());
+        if ($(window).width() >= 1200) {
+            for (var i = 1; i < 175; i++) {
+                var fra = i;
+                // console.log(fra.toString().length);
+                if (fra.toString().length == 1) {
+                    frame = "00" + fra;
+                } else if (fra.toString().length == 2) {
+                    frame = "0" + fra;
+                } else if (fra.toString().length == 3) {
+                    frame = fra;
+                }
+                var imgSeqAddress = "/UNLTRASTABLE-flip-pngSeq/tokenFlip-video0" + frame + ".png";
+                $("#coin").attr("src", imgSeqAddress);
+                await timer(0.00001); //because anim is in 24 fps so 60/24 = 2.5
+            }
+            console.log("done");
+        }
+        else{
+            $("#coin").attr("src", "/UNLTRASTABLE-flip-pngSeq/tokenFlip-video0001.png");
+        }
     }
 );
