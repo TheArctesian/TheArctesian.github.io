a2e9402f87462047a95f5a83ed979836fbc5ca4c
TheArctesian <87273292+TheArctesian@users.noreply.github.com>
2021-10-08 12:40:39 +0800

    added this shit

--- imgseq.js
+++ imgseq.js
@@ -20,28 +20,28 @@ $('#seq').hover(
     }
 );
 
-// $('#coin').hover(
-//     async function () {
-//         console.log($(window).width());
-//         if ($(window).width() >= 1200) {
-//             for (var i = 1; i < 175; i++) {
-//                 var fra = i;
-//                 // console.log(fra.toString().length);
-//                 if (fra.toString().length == 1) {
-//                     frame = "00" + fra;
-//                 } else if (fra.toString().length == 2) {
-//                     frame = "0" + fra;
-//                 } else if (fra.toString().length == 3) {
-//                     frame = fra;
-//                 }
-//                 var imgSeqAddress = "/UNLTRASTABLE-flip-pngSeq/tokenFlip-video0" + frame + ".png";
-//                 $("#coin").attr("src", imgSeqAddress);
-//                 await timer(0.00001); //because anim is in 24 fps so 60/24 = 2.5
-//             }
-//             console.log("done");
-//         }
-//         else{
-//             $("#coin").attr("src", "/UNLTRASTABLE-flip-pngSeq/tokenFlip-video0001.png");
-//         }
-//     }
-// );
+$('#coin').hover(
+    async function () {
+        console.log($(window).width());
+        if ($(window).width() >= 1200) {
+            for (var i = 1; i < 175; i++) {
+                var fra = i;
+                // console.log(fra.toString().length);
+                if (fra.toString().length == 1) {
+                    frame = "00" + fra;
+                } else if (fra.toString().length == 2) {
+                    frame = "0" + fra;
+                } else if (fra.toString().length == 3) {
+                    frame = fra;
+                }
+                var imgSeqAddress = "/UNLTRASTABLE-flip-pngSeq/tokenFlip-video0" + frame + ".png";
+                $("#coin").attr("src", imgSeqAddress);
+                await timer(0.00001); //because anim is in 24 fps so 60/24 = 2.5
+            }
+            console.log("done");
+        }
+        else{
+            $("#coin").attr("src", "/UNLTRASTABLE-flip-pngSeq/tokenFlip-video0001.png");
+        }
+    }
+);
