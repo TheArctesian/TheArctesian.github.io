66c86d7b2cc80caca974f9d54fc24d944a0ccc7b
TheArctesian <87273292+TheArctesian@users.noreply.github.com>
2021-10-08 12:59:57 +0800

    fixed 3d cumming effect

--- horny.js
+++ horny.js
@@ -1,12 +1,26 @@
 let constrain = 20;
 let mouseOverContainer = document.getElementById("ex1");
 let ex1Layer = document.getElementById("ex1-layer");
+let txt = document.getElementById("txt")
 
 function transforms(x, y, el) {
-    let box = el.getBoundingClientRect();
-    let calcX = -(y - box.y - (box.height / 2)) / constrain;
-    let calcY = (x - box.x - (box.width / 2)) / constrain;
-
+    let img = el.getBoundingClientRect();
+    let calcX = -(y - img.y - (img.height / 2)) / constrain;
+    let calcY = (x - img.x - (img.width / 2)) / constrain;
+    if( calcY >= 25 ){
+        calcY = 25;
+    }
+    if( calcY <= -20){
+        calcY = -20;
+    }
+    if( calcX >= 20){
+        calcX = 20;
+    }
+    if( calcX <= -10){
+        calcX = -10;
+    }
+    txt.textContent = "Rotation is: " + "   rotateX (" + calcX + "deg) " +
+        "   rotateY (" + calcY + "deg) ";
     return "perspective(100px) " +
         "   rotateX(" + calcX + "deg) " +
         "   rotateY(" + calcY + "deg) ";
